fd5050ebbdfd24ad796f166c5612cf2131f27f33
stiffgiit <233856@student.krakow.uek.pl>
2025-04-09 21:00:09 +0200

    Lab 4.3

--- EShop.Domain/Repositories/DataContext.cs
+++ EShop.Domain/Repositories/DataContext.cs
@@ -2,12 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using EShop.Domain.Models;
+
 
 namespace EShop.Domain.Repositories
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
